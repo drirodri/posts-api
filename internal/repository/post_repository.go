6945947e217ab9c8b6f958ae9ed08f910fe85c0a
drirodri <adriano.rsilva97@gmail.com>
2025-06-09 18:47:24 -0300

    feat: added interface to repository

--- internal/repository/post_repository.go
+++ internal/repository/post_repository.go
@@ -1,2 +1,15 @@
 // post_repository.go - Post data access layer
 package repository
+
+import (
+	"posts-api/internal/models"
+)
+type PostRepository interface {
+	CreatePost(post *models.Post) error
+	GetPostByID(id int64) (*models.Post, error)
+	GetAllPosts() ([]*models.Post, error)
+	UpdatePost(post *models.Post) error
+	DeletePost(id int64) error
+	GetByAuthorID(author string) ([]*models.Post, error)
+	GetTotalPosts() (int64, error)
+}
