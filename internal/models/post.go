1d735aef21f383d16210046d9adf2fa26f23b0e4
drirodri <adriano.rsilva97@gmail.com>
2025-06-09 14:35:11 -0300

    setup: setting up models

--- internal/models/post.go
+++ internal/models/post.go
@@ -1,1 +1,12 @@
 // post.go - Post model definition
+package models 
+
+type Post struct {
+	ID        int64  `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title     string `json:"title" gorm:"not null"`
+	Content   string `json:"content" gorm:"not null"`
+	AuthorID  int64  `json:"author_id" gorm:"not null"`
+	Author    User   `json:"author" gorm:"foreignKey:AuthorID;references:ID"`
+	CreatedAt string `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt string `json:"updated_at" gorm:"autoUpdateTime"`
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,10 @@
 // user.go - User reference model for Users API integration
+package models
+type User struct {
+	ID        int64  `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name  string `json:"username" gorm:"unique;not null"`
+	Email     string `json:"email" gorm:"unique;not null"`
+	Password  string `json:"password" gorm:"not null"`
+	CreatedAt string `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt string `json:"updated_at" gorm:"autoUpdateTime"`
+}
